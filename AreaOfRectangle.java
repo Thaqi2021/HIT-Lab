9a4940091d79443e3d73e424f96cba6540e756aa
Thaqi2021 <83491185+Thaqi2021@users.noreply.github.com>
2021-06-08 23:08:00 +0530

    Add files via upload

---
+++ AreaOfRectangle.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class AreaOfRectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    System.out.println("Enter the Lenght of rectangle :");
+	    Scanner sc = new Scanner(System.in);
+	    double l = sc.nextDouble();
+	    System.out.println("Enter the breath of rectangle :");
+	    double b = sc.nextDouble();
+	    double a = l*b;
+        System.out.println("your Area of Reactangle is :"+a);
+
+	}
+
+}
---
+++ SquareAreaDemo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class SquareAreaDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the Lenght of Square :");
+		Scanner sc = new Scanner(System.in);
+		int l = sc.nextInt();
+		int a = l*l;
+		System.out.println("Your Area of Square is : "+a);
+
+	}
+
+}
