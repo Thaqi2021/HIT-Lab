6c1a8b0732508fe6da655268e258db7136ab1a12
Thaqi2021 <83491185+Thaqi2021@users.noreply.github.com>
2021-07-19 13:50:30 +0530

    Add files via upload

---
+++ HIT_Project/src/TestingProject/BulkMail.java
@@ -0,0 +1,12 @@
+package TestingProject;
+
+public class BulkMail {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a ="thaqicm@gmail.com";
+		String [] b = a.split("@",2);
+		System.out.println(b);
+	}
+
+}
