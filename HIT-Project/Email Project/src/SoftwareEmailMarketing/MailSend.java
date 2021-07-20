9776e40f3fb4ed6fe82bfe6ee87ed5d1a47b2124
Thaqi2021 <83491185+Thaqi2021@users.noreply.github.com>
2021-07-20 13:59:52 +0530

    Update MailSend.java

--- HIT-Project/Email Project/src/SoftwareEmailMarketing/MailSend.java
+++ HIT-Project/Email Project/src/SoftwareEmailMarketing/MailSend.java
@@ -1,23 +1,16 @@
 package SoftwareEmailMarketing;
 
 import java.io.IOException;
-import java.util.Properties;
 
-import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
 import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
+
 
 
 public class MailSend {
 
 
-	public static void main(String[] args) throws AddressException, MessagingException {
+	public static void main(String[] args) throws AddressException, MessagingException{
 
 		EmailSender ar = new EmailSender();
 		try {
@@ -27,5 +20,4 @@ public class MailSend {
 		}
 	}
 	
-
 }
