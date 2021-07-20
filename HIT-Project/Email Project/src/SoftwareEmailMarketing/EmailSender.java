60c5e4324bd0a8726b1096a704227aeec543d80e
Thaqi2021 <83491185+Thaqi2021@users.noreply.github.com>
2021-07-20 13:52:35 +0530

    Update EmailSender.java

--- HIT-Project/Email Project/src/SoftwareEmailMarketing/EmailSender.java
+++ HIT-Project/Email Project/src/SoftwareEmailMarketing/EmailSender.java
@@ -11,7 +11,7 @@ import javax.mail.internet.AddressException;
 
 public class EmailSender {
 	void emailRecipiens() throws IOException, AddressException, MessagingException{
-		MailSend mail1 = new  MailSend();
+		EmailSender mail1 = new  EmailSender();
 		
 //		Map<String ,String> hm = new HashMap();
 //		hm.put("zaidmje@gmail.com", "https://github.com/MohammedZaidJ/HIT-lab");
@@ -39,6 +39,51 @@ public class EmailSender {
 				System.out.println("InValid Email id found :"+mail);
 			}
 		}
+	}
+	Session newSession = null;
+	MimeMessage mimemessage = null;
+	void sendEmail() throws MessagingException {
+		 String fromUser ="thaqifullstack@gmail.com";
+		 String fromUserPassword = "password";
+		 String emailHost="smtp.gmail.com";
+		 Transport transport = newSession.getTransport("smtp");
+		 transport.connect(emailHost,fromUser,fromUserPassword);
+		 transport.sendMessage(mimemessage, mimemessage.getAllRecipients());
+		 transport.close();
+		 System.out.println("\tEmail Sucessfully sent");
+	}
+
+	MimeMessage draftEmail(String name, String gitLink, String mailId) throws AddressException,MessagingException{
+		
+		String emailSubject ="A quick hello & congrats on the joining the HIT Team!";
+		String Signature ="<b style=\"color:red;\">Thanks & Regards</b><br>Mr Mohammed Thaqi<br>"
+				+ "FullStack Development Trainee,<br>"
+				+ "Haaris Infotech Pvt. Ltd.,<br>"
+				+ "E-Mail:thaqi.fullstack@gmail.com,<br>"
+				+ "Phone: +917845014181.";
+		String emailBody="Dear Mr/Mrs "+name+ "<br><br>To Know More Of Your Account  "+ "<a href="+gitLink+"/>"+gitLink+"</a><br><br>"+Signature;
+		
+		mimemessage = new MimeMessage(newSession);
+
+		mimemessage.addRecipient(Message.RecipientType.TO,new InternetAddress(mailId));	
+		mimemessage.setSubject(emailSubject);
+		MimeBodyPart bodypart = new MimeBodyPart();
+		bodypart.setContent(emailBody,"text/html");
+		
+		MimeMultipart multipart = new MimeMultipart();
+		multipart.addBodyPart(bodypart); 
+		mimemessage .setContent(multipart);
+		return mimemessage;
+
+	}
+
+	 void setupServerProperties() {
+		Properties properties = new Properties();
+		properties.put("mail.smtp.port", "587");
+		properties.put("mail.smtp.auth", "true");
+		properties.put("mail.smtp.starttls.enable","true");
+		newSession = Session.getDefaultInstance(properties,null);
+		
 	}
 
 }
