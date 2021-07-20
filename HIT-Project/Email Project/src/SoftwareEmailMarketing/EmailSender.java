aa9df988b2afb67a545a186d2e654cacc247cb7a
Thaqi2021 <83491185+Thaqi2021@users.noreply.github.com>
2021-07-20 14:01:08 +0530

    Update EmailSender.java

--- HIT-Project/Email Project/src/SoftwareEmailMarketing/EmailSender.java
+++ HIT-Project/Email Project/src/SoftwareEmailMarketing/EmailSender.java
@@ -6,8 +6,15 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
+import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
 import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 public class EmailSender {
 	void emailRecipiens() throws IOException, AddressException, MessagingException{
@@ -44,7 +51,7 @@ public class EmailSender {
 	MimeMessage mimemessage = null;
 	void sendEmail() throws MessagingException {
 		 String fromUser ="thaqifullstack@gmail.com";
-		 String fromUserPassword = "password";
+		 String fromUserPassword = "pass****";
 		 String emailHost="smtp.gmail.com";
 		 Transport transport = newSession.getTransport("smtp");
 		 transport.connect(emailHost,fromUser,fromUserPassword);
@@ -61,7 +68,7 @@ public class EmailSender {
 				+ "Haaris Infotech Pvt. Ltd.,<br>"
 				+ "E-Mail:thaqi.fullstack@gmail.com,<br>"
 				+ "Phone: +917845014181.";
-		String emailBody="Dear Mr/Mrs "+name+ "<br><br>To Know More Of Your Account  "+ "<a href="+gitLink+"/>"+gitLink+"</a><br><br>"+Signature;
+		String emailBody="Dear Mr/Mrs "+name+ "<br><br>To Know More Of Your Account : "+ "<a href="+gitLink+"/>"+gitLink+"</a><br><br>"+Signature;
 		
 		mimemessage = new MimeMessage(newSession);
 
@@ -85,5 +92,6 @@ public class EmailSender {
 		newSession = Session.getDefaultInstance(properties,null);
 		
 	}
+
 
 }
